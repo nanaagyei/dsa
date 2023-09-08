3193b91635a29075a60ef0222c74fc8fdf37a9f2
nanaagyei <tuffourp@oregonstate.edu>
2023-09-08 10:21:32 -0700

    koko eating bananas binary search

--- leetcode-grind75/koko_eating_banana.py
+++ leetcode-grind75/koko_eating_banana.py
@@ -14,3 +14,32 @@ class Solution:
                 k = min(i, k)
 
         return k
+
+    def minEatingSpeed2(self, piles, h: int) -> int:
+        # Binary Search
+        def possible(k):
+            return sum((p - 1) // k + 1 for p in piles) <= h
+
+        lo, hi = 1, max(piles)
+        while lo < hi:
+            mi = (lo + hi) // 2
+            if not possible(mi):
+                lo = mi + 1
+            else:
+                hi = mi
+        return lo
+
+    def minEatingSpeed3(self, piles, h: int) -> int:
+        # Binary Search
+        left, right = 1, max(piles)
+        res = right
+
+        while left <= right:
+            mid = (left + right) // 2
+            if sum([math.ceil(p / mid) for p in piles]) <= h:
+                res = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return res
