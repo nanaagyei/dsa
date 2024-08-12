909bb358194dfce63443b9212054c4a80ddcdf68
nanaagyei <tuffourp@oregonstate.edu>
2024-08-12 14:18:42 -0700

    leetcode problem updates

---
+++ leetcode-problems/generate_parentheses.py
@@ -0,0 +1,25 @@
+"""
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+"""
+
+class Solution:
+    def generateParentheses(self, n: int) -> list:
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+            
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
