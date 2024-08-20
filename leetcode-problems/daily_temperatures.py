07a3ee9399e08f9de2c6271fe0eddd2e53a57f06
nanaagyei <tuffourp@oregonstate.edu>
2024-08-19 19:21:58 -0700

    leetcode problem updates

--- leetcode-problems/daily_temperatures.py
+++ leetcode-problems/daily_temperatures.py
@@ -5,6 +5,15 @@ to wait after the ith day to get a warmer temperature.
 If there is no future day for which this is possible, keep answer[i] == 0 instead.
 """
 
+
 class Solution:
     def dailyTemperatures(self, temperatures: list) -> list:
-        pass
+        result = [0] * len(temperatures)
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
+                stackValue, stackIdx = stack.pop()
+                result[stackIdx] = i - stackIdx
+            stack.append((temp, i))
+        return result
