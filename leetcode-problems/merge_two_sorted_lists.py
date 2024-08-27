761402d493765c321a50091e6530bcc19240d035
nanaagyei <tuffourp@oregonstate.edu>
2024-08-27 14:53:33 -0700

    leetcode problem updates

---
+++ leetcode-problems/merge_two_sorted_lists.py
@@ -0,0 +1,31 @@
+"""
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+
+The new list should be made up of nodes from list1 and list2.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        dummy_node = ListNode()
+        node = dummy_node
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+        
+        node.next = list1 or list2
+
+        return dummy_node.next
---
+++ leetcode-problems/reverse_linked_list.py
@@ -0,0 +1,22 @@
+"""
+Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        temp = head
+        
+        while temp:
+            next_node = temp.next
+            temp.next = prev
+            prev = temp
+            temp = next_node
+        
+        return prev
