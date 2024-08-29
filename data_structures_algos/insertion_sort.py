a46bd02ed26fcbdd67c3b9190bc1b93275192d84
nanaagyei <tuffourp@oregonstate.edu>
2024-08-29 12:48:34 -0700

    leetcode problem updates

--- data_structures_algos/insertion_sort.py
+++ data_structures_algos/insertion_sort.py
@@ -1,10 +1,13 @@
 
 def insertion_sort(arr): 
     for i in range(1, len(arr)):
-        key = arr[i] # Assign the value of the current element to key
-        j = i - 1 # Assign the index of the previous element to j
-        while j >= 0 and key < arr[j]: # Loop through the array from the last element to the first element
-            arr[j + 1] = arr[j] # Shift the elements to the right
-            j -= 1 # Decrease the index of the previous element by 1
-        arr[j + 1] = key # Insert the key into the array
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            swap(j, j-1, arr)
+            j -= 1
+    
     return arr
+
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
