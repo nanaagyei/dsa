2f763de5599bd23c4261d408a6ec3a3b2d479df5
nanaagyei <tuffourp@oregonstate.edu>
2024-09-03 17:35:08 -0700

    leetcode problem updates

---
+++ leetcode-problems/remove_nth_node.py
@@ -0,0 +1,29 @@
+"""
+You are given the beginning of a linked list head, and an integer n.
+
+Remove the nth node from the end of the list and return the beginning of the list.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0)
+        left = dummy
+        right = head
+
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+        
+        while right:
+            left = left.next
+            right = right.next
+        
+        left.next = left.next.next
+
+        return dummy.next
