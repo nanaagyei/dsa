8015779809cc77cd9fb302ef4f1882dfa4892c5f
nanaagyei <tuffourp@oregonstate.edu>
2025-07-12 14:51:20 -0500

    update dsa questions

---
+++ leetcode-problems/setMatrixZeros.py
@@ -0,0 +1,70 @@
+"""
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
+ 
+
+Follow up:
+
+A straightforward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+"""
+
+from typing import List
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows, cols = len(matrix), len(matrix[0])
+        rowZeros = False
+
+        # Check if the first row has any zeros
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZeros = True
+
+        # Set zeros for the rest of the matrix
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+
+        # Set zeros for the first row and column if needed
+        if matrix[0][0] == 0:
+            for r in range(rows):
+                matrix[r][0] = 0
+
+        if rowZeros:
+            for c in range(cols):
+                matrix[0][c] = 0
--- leetcode-problems/valid_anagram.py
+++ leetcode-problems/valid_anagram.py
@@ -10,6 +10,7 @@ letters exactly once.
 from collections import Counter
 # Solution 1
 
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -21,13 +22,23 @@ class Solution:
         for c in countS:
             if countS[c] != countT.get(c, 0):
                 return False
-        
+
         return True
-    
+
     # Solution 2
     def isAnagram2(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
-    
+
     # Solution 3
     def isAnagram3(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+
+    # Solution 4
+    def isAnagram4(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = [0] * 26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        return all(x == 0 for x in count)
